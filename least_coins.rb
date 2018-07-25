1dc8218a82fc5d274754de402a2cd65fc927c042
Nancerss <nancy.alquicirauc@gmail.com>
2018-07-25 21:01:40 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -4,12 +4,20 @@ def least_coins(cents)
   quarters = cents / 25
   if quarters == 0 
     coins [:quarters] = 0 
-  elsif quarter != 0 
+  elsif quarters != 0 
     coins [:quarters] = quarters
     cents = cents - quarters * 25 
-    
+  end 
+  dimes = cents / 10 
+  if dimes == 0 
+    coins [:dimes] = 0 
+  elsif dimes != 0 
+    coins [:dimes] = dimes 
+    cents = cents - dimes * 10 
   end 
 end 
+
+least_coins
   
   
 
