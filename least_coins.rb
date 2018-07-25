6984586938474403cf2dc218dcbfb9ce8eda7eb2
Nancerss <nancy.alquicirauc@gmail.com>
2018-07-25 20:40:32 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,13 @@
-#write out your code here
-
+write out your code here
 def least_coins(cents)
+  coins = {:quarters => 0, :dimes => 0, :nickels => 0, :pennies => 0}
+  if cents / 25 = 0 
+    puts :quarters = 0
+    
+  end 
+end 
+  
+  
 
 #Code your answer here!
 
-end
